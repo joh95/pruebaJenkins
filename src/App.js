18cfec49f9f9a4629d29c41e6ca4d8cde285e22e
MartinEliasQ <elias.quintero@udea.edu.co>
2019-03-07 13:23:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to CODEX</h1>
         </header>
-<iframe src="https://codexudea.github.io" width="100%">
+<iframe src="https://codexudea.github.io" width="80%" height="600px">
       
   <p>Your browser does not support iframes.</p>
 </iframe>
